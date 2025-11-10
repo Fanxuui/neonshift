7f23f77f94e7221fca1651e7b2604b397a5644d1
V-run64 <vpandian4u@gmail.com>
2025-11-10 16:01:13 -0500

    quit game command

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,8 +2,19 @@ extends Node
 
 func _ready() -> void:
 	print("GameManager ready")
+	
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		quit_game()
 
 # 玩家死亡时调用这个
 func restart_run() -> void:
 	GameState.reset()
 	get_tree().reload_current_scene() 
+	
+func quit_game() -> void:
+	print("Quitting game...")
+	get_tree().quit()
+	
+	
+	

776d63d668eebe37274acf7854fe9c3d3f1c9004
Fanxuui <xx2539@nyu.edu>
2025-11-10 16:40:47 -0500

    death behavior
    modification for better death behavior: add fading screen, fix some minor bugs

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,5 +5,9 @@ func _ready() -> void:
 
 # 玩家死亡时调用这个
 func restart_run() -> void:
+	var fade_rect = get_tree().current_scene.get_node("CanvasLayer/FadeRect")
+	if fade_rect:
+		await fade_rect.fade_to_black(1.0)
 	GameState.reset()
 	get_tree().reload_current_scene() 
+	
